e11780d3419209a59ad31665b68bf3f0a12c2518
Lau230595 <lvilcahu@unsa.edu.pe>
2025-05-11 13:16:21 -0500

    Agrego archivos del laboratorio de ajedrez

---
+++ Lab5-Python/lab04/src/matrices.py
@@ -0,0 +1,31 @@
+def esEscalar(m):
+    d = m[0][0]
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i != j:
+                if m[i][j] != 0:
+                    print("Elemento fuera de diagonal no es 0:", m[i][j])
+                    return False
+            elif m[i][j] != d:
+                print("Elemento diagonal no es igual a", d, ":", m[i][j])
+                return False
+    return True
+
+def esUnitaria(m):
+    return m[0][0] == 1 and esEscalar(m)
+
+# Ejemplo de uso
+matriz1 = [
+    [5, 0, 0],
+    [0, 5, 0],
+    [0, 0, 5]
+]
+
+matriz2 = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+
+print("¿matriz1 es escalar?", esEscalar(matriz1))
+print("¿matriz2 es unitaria?", esUnitaria(matriz2))
---
+++ Lab5-Python/lab04/src/matricesunitaria.py
@@ -0,0 +1,26 @@
+def esUnitaria(m):
+    for i in range(len(m)):
+        for j in range(len(m)):
+            if i == j:
+                if m[i][j] != 1:
+                    print(f"Elemento diagonal m[{i}][{j}] = {m[i][j]} no es 1")
+                    return False
+            else:
+                if m[i][j] != 0:
+                    print(f"Elemento fuera de la diagonal m[{i}][{j}] = {m[i][j]} no es 0")
+                    return False
+    return True
+matriz5 = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1]
+]
+
+matriz6 = [
+    [1, 0, 0],
+    [0, 2, 0],
+    [0, 0, 1]
+]
+
+print("¿matriz5 es unitaria?", esUnitaria(matriz5))
+print("¿matriz6 es unitaria?", esUnitaria(matriz6))

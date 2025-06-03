737fa26801ca90a1df85617434aa439a7dce02db
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-02 22:54:19 -0500

    Paso 4.3: Vistas para login, logout y registro implementadas

--- Lab07/usuarios/views.py
+++ Lab07/usuarios/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from .forms import FormularioRegistro
 
-# Create your views here.
+def registro_usuario(request):
+    if request.method == 'POST':
+        form = FormularioRegistro(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = FormularioRegistro()
+    return render(request, 'usuarios/registro.html', {'form': form})
+
+def login_usuario(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('inicio')
+        else:
+            return render(request, 'usuarios/login.html', {'error': 'Usuario o contraseña inválido'})
+    return render(request, 'usuarios/login.html')
+
+def logout_usuario(request):
+    logout(request)
+    return redirect('login')

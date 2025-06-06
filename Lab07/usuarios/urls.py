1e510422781cc19def4c9f9c3c354bbcac236ba8
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-06 17:09:41 -0500

    Corrección de plantilla index.html y uso correcto de static para imágenes, CSS y JS

--- Lab07/usuarios/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_usuario, name='login'),
-    path('logout/', views.logout_usuario, name='logout'),
-    path('registro/', views.registro_usuario, name='registro'),
-]

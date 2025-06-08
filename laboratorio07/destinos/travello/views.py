e21ace5d9b3df6ff77834c47f7ad2b5741cd156f
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-08 04:41:51 -0500

    Creacion de  modelo de tabla DestinosTuristicos

--- laboratorio07/destinos/travello/views.py
+++ laboratorio07/destinos/travello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
+from .models import DestinosTuristicos
 
-# Create your views here.
 def index(request):
-    return render(request, "index.html")
+    destinos = DestinosTuristicos.objects.all()
+    return render(request, "index.html", {"destinos": destinos})

d58aba8e4105d840a3e344fea4375558727d1cc9
J-Gabriel-Q-M <josquispem@unsa.edu.pe>
2025-06-29 11:25:09 -0500

    inyección del servicio WordService y reinicio al iniciar

--- Lab09/hangman-game/src/app/game/game.ts
+++ Lab09/hangman-game/src/app/game/game.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { WordService } from '../word';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.html',
   styleUrls: ['./game.css']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   alphabet: string[] = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ'.split('');
+  message: string = '';
+
+  constructor(public wordService: WordService) {}
+
+  ngOnInit(): void {
+    this.wordService.restart(); // Inicia la partida al cargar
+  }
 }

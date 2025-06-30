66bd65fba8432018cc2c790e6411395f431a5ea9
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-29 19:30:43 -0500

    modificacion de rutas en  el juego

--- Lab09/science-quiz/science-quiz/src/app/app.routes.ts
+++ Lab09/science-quiz/science-quiz/src/app/app.routes.ts
@@ -1,10 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home';
 import { QuestionComponent } from './question/question';
 import { ResultsComponent } from './results/results';
 
-export const routes: Routes = [
+const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'question/:id', component: QuestionComponent },
   { path: 'results', component: ResultsComponent }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }

073665989a47e0f51bfe11e35e3d59fc70eff62d
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-29 19:17:06 -0500

    modificacion de app.route para enrutamientos

--- Lab09/science-quiz/science-quiz/src/app/app.routes.ts
+++ Lab09/science-quiz/science-quiz/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { QuestionComponent } from './question/question';
+import { ResultsComponent } from './results/results';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'question/:id', component: QuestionComponent },
+  { path: 'results', component: ResultsComponent }
+];

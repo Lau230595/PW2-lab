d61c1f3f7dfb24ef1a8ee7f0d4a53b00ce9a79ed
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-29 19:55:07 -0500

    modificacion de main.ts

--- Lab09/science-quiz/science-quiz/src/main.ts
+++ Lab09/science-quiz/science-quiz/src/main.ts
@@ -1,13 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
-import { importProvidersFrom } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserModule),
-    provideRouter(routes)
-  ]
+  providers: [provideRouter(routes)]
 });

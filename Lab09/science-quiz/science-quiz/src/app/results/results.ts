83d4580e25b60985b6c1d20a519b481a0664b0ab
Lau230595 <lvilcahu@unsa.edu.pe>
2025-06-29 19:21:02 -0500

    modificacion de results.ts  para el juegos

--- Lab09/science-quiz/science-quiz/src/app/results/results.ts
+++ Lab09/science-quiz/science-quiz/src/app/results/results.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-results',
-  imports: [],
   templateUrl: './results.html',
-  styleUrl: './results.css'
 })
-export class Results {
+export class ResultsComponent {
+  score: number = 0;
 
+  constructor(private router: Router) {
+    const nav = this.router.getCurrentNavigation();
+    if (nav?.extras?.state) {
+      this.score = nav.extras.state['score'];
+    }
+  }
 }
